32ae4590af9d17a1009b7bcd3fa630c44ad0b830
Emily Pancake <epancake24@gmail.com>
2018-02-19 12:22:53 -0700

    remove console.logs

--- src/components/randomq.js
+++ src/components/randomq.js
@@ -5,17 +5,14 @@ import QuestionCard from './QuestionCard'
 
 const RandomQ = (props) => {
   const questions = props.questions
-  console.log('quesitions', questions)
   const randomid = () => {
     var rindex = Math.floor(Math.random() * (questions.length));
-    console.log('riindexd', rindex)
     return rindex
   }
 
   const rindex = randomid()
 
   const question = questions[rindex]
-  console.log('finalSingleQ', question)
 
   return <QuestionCard key={question.id + question.name}
     question={question}
